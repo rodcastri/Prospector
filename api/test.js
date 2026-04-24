4c1ead775e0da18dd54e1f79d680a7b22ddedac4
rodcastri <rodcastrillonm@gmail.com>
2026-04-24 12:40:34 -0500

    Add files via upload

--- api/test.js
+++ api/test.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   const key = process.env.GROQ_API_KEY;
--- api/youtube.js
+++ api/youtube.js
@@ -1,5 +1,5 @@
 // Extracts email from YouTube channel About page
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
